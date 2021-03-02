1369945314e522ecc6e5bf4c378d9db227a4994e
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-02 16:33:35 -0600

    feat: create setupRoutes() that will create a new gorilla mux router and have routes that utilize the functions defined in handlers.go and has http requests methods defined for each route to implement CRUD functionality

--- controllers/routes.go
+++ controllers/routes.go
@@ -1,1 +1,20 @@
+package controllers
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var prepend = "/api/v1/"
+
+func setupRoutes() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc(prepend+"/", getPeople).Methods("GET")
+	router.HandleFunc(prepend+"/person/{id}", getPerson).Methods("GET")
+	router.HandleFunc(prepend+"/person", createPerson).Methods("POST")
+	router.HandleFunc(prepend+"/person/{id}", updatePerson).Methods("PUT")
+	router.HandleFunc(prepend+"/person/{id}", deletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
