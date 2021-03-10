31b233307269a5f058e2839ef352666eb932ed11
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-10 14:33:22 -0600

    feat: refactor TestGetPerson() to now test for ID values of 1 and 2 utlizing conditionals to check the returned recorded response body to the expected string value.

--- controllers/handlers_test.go
+++ controllers/handlers_test.go
@@ -40,38 +40,34 @@ func TestGetPeople(t *testing.T) {
 }
 
 func TestGetPerson(t *testing.T) {
-	req, err := http.NewRequest("GET", prepend+"/person/1", nil)
-	if err != nil {
-		t.Fatal(err)
+	IDs := []string{"1", "2"}
+	for _, id := range IDs {
+		req, err := http.NewRequest("GET", prepend+"/person/"+id, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("TestGetPerson(): %v\n", req.URL)
+		respRec := httptest.NewRecorder()
+		handler := http.HandlerFunc(getPerson)
+		handler.ServeHTTP(respRec, req)
+
+		if status := respRec.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		if id == "1" {
+			expected := `{"id":1,"name":"Caleb","age":26,"created_at":"2021-03-04T15:05:12Z","updated_at":"2021-03-04T15:16:37Z"}`
+			if strings.TrimRight(respRec.Body.String(), "\n") != expected {
+				t.Errorf("handler returned unexpected body: got %v want %v", respRec.Body.String(), expected)
+			}
+		}
+
+		if id == "2" {
+			expected := `{"id":2,"name":"Brad","age":25,"created_at":"2021-03-04T15:16:05Z","updated_at":"2021-03-04T15:16:05Z"}`
+			if strings.TrimRight(respRec.Body.String(), "\n") != expected {
+				t.Errorf("handler returned unexpected body: got %v want %v", respRec.Body.String(), expected)
+			}
+		}
 	}
-	fmt.Printf("TestGetPerson(): %v\n", req.URL)
-	respRec := httptest.NewRecorder()
-	handler := http.HandlerFunc(getPerson)
-	handler.ServeHTTP(respRec, req)
-
-	if status := respRec.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	// if id == "1" {
-	expected := `{"id":1,"name":"Caleb","age":26,"created_at":"2021-03-04T15:05:12Z","updated_at":"2021-03-04T15:16:37Z"}`
-	if strings.TrimRight(respRec.Body.String(), "\n") != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", respRec.Body.String(), expected)
-	}
-	// }
-	// }
-
-	// respRec := httptest.NewRecorder()
-
-	// SetupRoutes().ServeHTTP(respRec, req)
-	// if status := respRec.Code; status != http.StatusOK {
-	// 	t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	// }
-
-	//Check the response body is what we expect
-	// expected := `[{"id":1,"name":"Caleb","age":26,"created_at":"2021-03-04T15:05:12Z","updated_at":"2021-03-04T15:16:37Z"}]`
-	// if strings.TrimRight(respRec.Body.String(), "\n") != expected {
-	// 	t.Errorf("handler returned unexpected body: got %v want %v", respRec.Body.String(), expected)
-	// }
 
 }
